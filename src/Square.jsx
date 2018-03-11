610306be939bfc4ef332d431d2d39dd10eb7a3a3
James Kim <yameskim@gmail.com>
2018-03-11 17:41:56 -0400

    Split components to separate files

---
+++ src/Square.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// This is a functional component:
+// Creates each square cell and attached onClick event listener.
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+export default Square;
