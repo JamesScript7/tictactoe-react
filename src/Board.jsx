fc51ff5c10f9ca6f05e4d28c39a8dc7ed8d797ec
James Kim <yameskim@gmail.com>
2018-03-11 18:11:06 -0400

    Add winner function

--- src/Board.jsx
+++ src/Board.jsx
@@ -3,6 +3,32 @@ import React, { Component } from 'react';
 // COMPONENTS:
 import Square from './Square';
 
+// Helper function that finds out if there is a winner.
+function calcWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    // New way to set variables.
+    const [a,b,c] = lines[i];
+
+    // Asks does square[a] have value? if it does then does the combination evals to true.
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 class Board extends React.Component {
   constructor(props) {
     super(props);
@@ -17,6 +43,14 @@ class Board extends React.Component {
   // 3. Sets new array with updated 'X' or 'O' from original null value.
   handleClick(i) {
     const squares = this.state.squares.slice();
+
+    // Returns early and ignore the click if:
+    // 1. someone won
+    // 2. if square is filled.
+    if (calcWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
 
     this.setState({
@@ -34,7 +68,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calcWinner(this.state.squares);
+    let status;
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    }
 
     return (
       <div>
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // This is a functional component:
 // Creates each square cell and attached onClick event listener.
